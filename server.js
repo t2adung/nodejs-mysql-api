4b8ccb13bd177487f2afac2f1e2e211b70c58dd2
Dung Tran <t2adung@gmail.com>
2022-03-16 09:00:03 +0700

    init node express

---
+++ server.js
@@ -0,0 +1,17 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('hello')
+});
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4001;
+app.listen(port, () => console.log('Server listening on port ' + port));
