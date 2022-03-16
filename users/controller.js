b8719d1aaf2f3de696d924dd62dd3554d17c49eb
Dung Tran <t2adung@gmail.com>
2022-03-16 16:11:21 +0700

    update delet eusre

--- users/controller.js
+++ users/controller.js
@@ -8,6 +8,10 @@ const userService = require('./service');
 router.post('/register', registerSchema, register);
 router.post('/authenticate', authenticateSchema, authenticate);
 router.get('/', authorize(), getAll);
+router.get('/current', authorize(), getCurrent);
+router.get('/:id', authorize(), getById);
+router.put('/:id', authorize(), updateSchema, update);
+router.delete('/:id', authorize(), _delete);
 
 module.exports = router;
 
@@ -44,5 +48,37 @@ function authenticate(req, res, next) {
 function getAll(req, res, next) {
     userService.getAll()
         .then(users => res.json(users))
+        .catch(next);
+}
+
+function getCurrent(req, res, next) {
+    res.json(req.user);
+}
+
+function getById(req, res, next) {
+    userService.getById(req.params.id)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function updateSchema(req, res, next) {
+    const schema = Joi.object({
+        firstName: Joi.string().empty(''),
+        lastName: Joi.string().empty(''),
+        username: Joi.string().empty(''),
+        password: Joi.string().min(6).empty('')
+    });
+    validateRequest(req, next, schema);
+}
+
+function update(req, res, next) {
+    userService.update(req.params.id, req.body)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function _delete(req, res, next) {
+    userService.delete(req.params.id)
+        .then(() => res.json({ message: 'User deleted successfully' }))
         .catch(next);
 }
--- users/service.js
+++ users/service.js
@@ -7,6 +7,10 @@ module.exports = {
     create,
     authenticate,
     getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
 };
 
 async function create(params) {
@@ -37,6 +41,36 @@ async function authenticate({ username, password }) {
 
 async function getAll() {
     return await db.User.findAll();
+}
+
+async function getById(id) {
+    return await getUser(id);
+}
+
+async function update(id, params) {
+    const user = await getUser(id);
+
+    // validate
+    const usernameChanged = params.username && user.username !== params.username;
+    if (usernameChanged && await db.User.findOne({ where: { username: params.username } })) {
+        throw 'Username "' + params.username + '" is already taken';
+    }
+
+    // hash password if it was entered
+    if (params.password) {
+        params.hash = await bcrypt.hash(params.password, 10);
+    }
+
+    // copy params to user and save
+    Object.assign(user, params);
+    await user.save();
+
+    return omitHash(user.get());
+}
+
+async function _delete(id) {
+    const user = await getUser(id);
+    await user.destroy();
 }
 
 // helper functions
