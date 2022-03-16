64401be12cc40206e63e1490d269ac960cf2aaf6
Dung Tran <t2adung@gmail.com>
2022-03-16 10:48:00 +0700

    connect mysql db

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const db = require('_helpers/db');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
